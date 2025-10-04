78f9d6c5a6fa862de4bf1eab87c2d93bb2f20503
Mirbek <mirbekhalilov470@gmail.com>
2025-10-04 16:35:06 +0600

    fix Vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/' // обязательно, чтобы пути к скриптам были корректные
 })
